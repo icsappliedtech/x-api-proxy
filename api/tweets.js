78dfd84bda7133f4401ca291f4dc6676e58b7911
Jason Weimer <125435942+icsappliedtech@users.noreply.github.com>
2025-03-03 07:01:25 +0700

    Create tweets.js

---
+++ api/tweets.js
@@ -0,0 +1,15 @@
+javascript
+
+module.exports = async (req, res) => {
+  const fetch = require('node-fetch');
+  try {
+    const response = await fetch('https://api.twitter.com/2/tweets/search/recent?query=techeducation&tweet.fields=text', {
+      headers: { 'Authorization': `Bearer ${process.env.X_BEARER_TOKEN}` }
+    });
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch tweets', details: error.message });
+  }
+};
+
